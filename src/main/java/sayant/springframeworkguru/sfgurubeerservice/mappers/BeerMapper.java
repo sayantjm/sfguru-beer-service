5c2505b31e326b27e946cd43eb34dc55fb5a4cca
Juanma Perales <sayantjm@gmail.com>
2020-05-31 20:02:28 +0200

    Adding mapstruct for conversions. Closes #10

---
+++ src/main/java/sayant/springframeworkguru/sfgurubeerservice/mappers/BeerMapper.java
@@ -0,0 +1,15 @@
+package sayant.springframeworkguru.sfgurubeerservice.mappers;
+
+import org.mapstruct.Mapper;
+import sayant.springframeworkguru.sfgurubeerservice.domain.Beer;
+import sayant.springframeworkguru.sfgurubeerservice.model.BeerDto;
+
+/**
+ * Created by sayantjm on 31/5/20
+ */
+@Mapper(uses = DateMapper.class)
+public interface BeerMapper {
+    BeerDto BeerToBeerDto(Beer beer);
+
+    Beer BeerDtoToBeer(BeerDto beerDto);
+}
