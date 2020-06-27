dcf6b676d1cb18c8dbe45fd98f41323d3785a084
Juanma Perales <sayantjm@gmail.com>
2020-06-27 18:32:36 +0200

    Adding Jackson config for BeerDTO. Closes #13

--- src/main/java/sayant/springframeworkguru/sfgurubeerservice/model/BeerDto.java
+++ src/main/java/sayant/springframeworkguru/sfgurubeerservice/model/BeerDto.java
@@ -1,5 +1,6 @@
 package sayant.springframeworkguru.sfgurubeerservice.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,9 +29,11 @@ public class BeerDto {
     @Null
     private Integer version;
 
+    @JsonFormat(pattern="yyyy-MM-dd'T:HH:mm:ssZ", shape= JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime createdDate;
 
+    @JsonFormat(pattern="yyyy-MM-dd'T:HH:mm:ssZ", shape= JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime updatedDate;
 
@@ -44,6 +47,7 @@ public class BeerDto {
     @NotNull
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
